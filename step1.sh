b4019ecc397ae84ea139c213103738fe16cced52
ContratopDev <miandroidtabletfirst@gmail.com>
2023-01-22 20:39:38 +0100

    dev

--- step1.sh
+++ step1.sh
@@ -3,7 +3,6 @@
 cd $HOME
 pkg install proot-distro -y
 proot-distro install debian
-proot-distro login debian
 
 
 echo "Deseas iniciar sesion automaticamente en debian al abrir Termux? (Y/N)"
@@ -11,8 +10,11 @@ read -r answer
 if [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
     echo "Datos guardados"
     echo "proot-distro login debian" >> $HOME/.bashrc
+    proot-distro login debian
+    exit
 elif [ "$answer" = "N" ] || [ "$answer" = "n" ]; then
     clear
+    proot-distro login debian
     exit
 else
     echo "Opcion invalida"
